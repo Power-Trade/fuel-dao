decf0a720b29ad4edca675b868b5a34ce118f864
Vincent de Almeida <vince0656@googlemail.com>
2020-09-07 15:38:06 +0100

    Simple vesting contract without delegatin

---
+++ contracts/VestingContractWithoutDelegation.sol
@@ -0,0 +1,171 @@
+pragma solidity ^0.5.16;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "./IERC20.sol";
+
+contract VestingContractWithoutDelegation is ReentrancyGuard {
+    using SafeMath for uint256;
+
+    event ScheduleCreated(address indexed _beneficiary, uint256 indexed _amount);
+
+    event DrawDown(address indexed _beneficiary, uint256 indexed _amount, uint256 indexed _time);
+
+    uint256 public start;
+    uint256 public end;
+    uint256 public cliffDuration;
+
+    address public owner;
+
+    // Vested address to its schedule
+    mapping(address => uint256) public vestedAmount;
+    mapping(address => uint256) public totalDrawn;
+    mapping(address => uint256) public lastDrawnAt;
+
+    IERC20 public token;
+
+    // All durations are based in number days
+    uint256 public constant PERIOD_ONE_DAY_IN_SECONDS = 1 days;
+
+    constructor(IERC20 _token, uint256 _start, uint256 _end, uint256 _cliffDuration) public {
+        require(address(_token) != address(0));
+        owner = msg.sender;
+        token = _token;
+        start = _start;
+        end = _end;
+        cliffDuration = _cliffDuration;
+    }
+
+    function createVestingSchedules(address[] calldata _beneficiaries, uint256[] calldata _amounts) external returns (bool) {
+        require(_beneficiaries.length > 0, "VestingContract::createVestingSchedules: Empty Data");
+        require(_beneficiaries.length == _amounts.length, "VestingContract::createVestingSchedules: Array lengths do not match");
+
+        bool result = true;
+
+        for(uint i = 0; i < _beneficiaries.length; i++) {
+            address beneficiary = _beneficiaries[i];
+            uint256 amount = _amounts[i];
+            _createVestingSchedule(beneficiary, amount);
+        }
+
+        return result;
+    }
+
+    function createVestingSchedule(address _beneficiary, uint256 _amount) external returns (bool) {
+        return _createVestingSchedule(_beneficiary, _amount);
+    }
+
+    function drawDown() nonReentrant external returns (bool) {
+        return _drawDown(msg.sender);
+    }
+
+    ///////////////
+    // Accessors //
+    ///////////////
+
+    // for a given beneficiary
+    function tokenBalance() external view returns (uint256) {
+        return token.balanceOf(msg.sender);
+    }
+
+    function vestingScheduleForBeneficiary(address _beneficiary)
+    external view
+    returns (uint256 _amount, uint256 _totalDrawn, uint256 _lastDrawnAt, uint256 _remainingBalance) {
+        return (
+        vestedAmount[_beneficiary],
+        totalDrawn[_beneficiary],
+        lastDrawnAt[_beneficiary],
+        vestedAmount[_beneficiary].sub(totalDrawn[_beneficiary])
+        );
+    }
+
+    function availableDrawDownAmount(address _beneficiary) external view returns (uint256 _amount, uint256 _timeLastDrawn, uint256 _drawDownRate) {
+        return _availableDrawDownAmount(_beneficiary);
+    }
+
+    function remainingBalance(address _beneficiary) external view returns (uint256) {
+        return vestedAmount[_beneficiary].sub(totalDrawn[_beneficiary]);
+    }
+
+    //////////////
+    // Internal //
+    //////////////
+    function _createVestingSchedule(address _beneficiary, uint256 _amount) internal returns (bool) {
+        require(_beneficiary != address(0), "VestingContract::createVestingSchedule: Beneficiary cannot be empty");
+        require(_amount > 0, "VestingContract::createVestingSchedule: Amount cannot be empty");
+
+        // Ensure one per address
+        require(vestedAmount[_beneficiary] == 0, "VestingContract::createVestingSchedule: Schedule already in flight");
+
+        vestedAmount[_beneficiary] = _amount;
+
+        // Vest the tokens into the deposit account and delegate to the beneficiary
+        require(token.transferFrom(msg.sender, address(this), _amount), "VestingContract::createVestingSchedule: Unable to escrow tokens");
+
+        emit ScheduleCreated(_beneficiary, _amount);
+
+        return true;
+    }
+
+    function _drawDown(address _beneficiary) internal returns (bool) {
+        require(vestedAmount[_beneficiary] > 0, "VestingContract::_drawDown: There is no schedule currently in flight");
+
+        (uint256 amount,,) = _availableDrawDownAmount(_beneficiary);
+        require(amount > 0, "VestingContract::_drawDown: No allowance left to withdraw");
+
+        // Update last drawn to now
+        lastDrawnAt[_beneficiary] = _getNow();
+
+        // Increase total drawn amount
+        totalDrawn[_beneficiary] = totalDrawn[_beneficiary].add(amount);
+
+        // Issue tokens to beneficiary
+        require(token.transfer(_beneficiary, amount), "VestingContract::_drawDown: Unable to transfer tokens");
+
+        emit DrawDown(_beneficiary, amount, _getNow());
+
+        return true;
+    }
+
+    function _getNow() internal view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function _availableDrawDownAmount(address _beneficiary) internal view returns (uint256 _amount, uint256 _timeLastDrawn, uint256 _drawDownRate) {
+        require(start <= _getNow(), "VestingContract::_availableDrawDownAmount: Schedule not started");
+
+        ///////////////////////
+        // Cliff Period      //
+        ///////////////////////
+
+        if (_getNow() < start.add(cliffDuration)) {
+            // the cliff period has not ended, no tokens to draw down
+            return (0, lastDrawnAt[_beneficiary], 0);
+        }
+
+        ///////////////////////
+        // Schedule complete //
+        ///////////////////////
+
+        if (_getNow() > end) {
+            uint256 amount = vestedAmount[_beneficiary].sub(totalDrawn[_beneficiary]);
+            return (amount, lastDrawnAt[_beneficiary], 0);
+        }
+
+        ////////////////////////
+        // Schedule is active //
+        ////////////////////////
+
+        // Work out when the last invocation was
+        uint256 timeLastDrawn = lastDrawnAt[_beneficiary] == 0 ? start : lastDrawnAt[_beneficiary];
+
+        // Find out how much time has past since last invocation
+        uint256 timePassedSinceLastInvocation = _getNow().sub(timeLastDrawn);
+
+        // Work out how many due tokens - time passed * rate per second
+        uint256 drawDownRate = vestedAmount[_beneficiary].div(end.sub(start));
+        uint256 amount = timePassedSinceLastInvocation.mul(drawDownRate);
+
+        return (amount, timeLastDrawn, drawDownRate);
+    }
+}
