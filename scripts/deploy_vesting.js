29ca346b4f12c267a73f0c1f65dbcb72dcf2bf23
Vincent de Almeida <vince0656@googlemail.com>
2020-09-09 11:38:17 +0100

    Deployment scrips for vesting with and without delegation

---
+++ scripts/deploy_vesting.js
@@ -0,0 +1,41 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const deployerAddress = await deployer.getAddress();
+    console.log(
+        "Deploying vesting contract with the account:",
+        deployerAddress
+    );
+
+    const syncTokenAddress = process.env.SYNC_TOKEN_ADDRESS;
+    console.log('Sync Token Address', syncTokenAddress);
+
+    const baseVestingDepositAccountAddress = process.env.BASE_DEPOSIT_ACCOUNT_ADDRESS;
+    console.log('Base vesting deposit account', baseVestingDepositAccountAddress);
+
+    const start = process.env.VESTING_START;
+    console.log('Start UNIX timestamp', start);
+
+    const end = process.env.VESTING_END;
+    console.log('End UNIX timestamp', end);
+
+    const cliffDuration = process.env.VESTING_CLIFF_DURATION_IN_SECONDS;
+    console.log('Cliff duration in seconds', cliffDuration);
+
+    const vestingContractFactory = await ethers.getContractFactory("VestingContract");
+    const vestingContract = await vestingContractFactory.deploy(
+        syncTokenAddress,
+        baseVestingDepositAccountAddress,
+        start,
+        end,
+        cliffDuration
+    );
+
+    console.log('Vesting contract deployed at:', (await vestingContract.deployed()).address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/deploy_vesting_without_delegation.js
@@ -0,0 +1,37 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const deployerAddress = await deployer.getAddress();
+    console.log(
+        "Deploying vesting contract without delegation with the account:",
+        deployerAddress
+    );
+
+    const syncTokenAddress = process.env.SYNC_TOKEN_ADDRESS;
+    console.log('Sync Token Address', syncTokenAddress);
+
+    const start = process.env.VESTING_START;
+    console.log('Start UNIX timestamp', start);
+
+    const end = process.env.VESTING_END;
+    console.log('End UNIX timestamp', end);
+
+    const cliffDuration = process.env.VESTING_CLIFF_DURATION_IN_SECONDS;
+    console.log('Cliff duration in seconds', cliffDuration);
+
+    const vestingContractFactory = await ethers.getContractFactory("VestingContractWithoutDelegation");
+    const vestingContract = await vestingContractFactory.deploy(
+        syncTokenAddress,
+        start,
+        end,
+        cliffDuration
+    );
+
+    console.log('Vesting contract without delegation deployed at:', (await vestingContract.deployed()).address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
