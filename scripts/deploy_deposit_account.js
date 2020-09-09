8a2086ce309699d258d1f5d9d95539a02cb56c29
Vincent de Almeida <vince0656@googlemail.com>
2020-09-09 12:40:33 +0100

    Deployment script for the vesting deposit account

---
+++ scripts/deploy_deposit_account.js
@@ -0,0 +1,20 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const deployerAddress = await deployer.getAddress();
+    console.log(
+        "Deploying deposit contract with the account:",
+        deployerAddress
+    );
+
+    const depositContractFactory = await ethers.getContractFactory("VestingDepositAccount");
+    const depositContract = await depositContractFactory.deploy();
+
+    console.log('Vesting deposit contract deployed at:', (await depositContract.deployed()).address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
