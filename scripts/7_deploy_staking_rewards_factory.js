3665d7f3f34f2f2add51b863fc21975733951980
Andy Gray <andy.gray@blockrocket.tech>
2020-09-30 17:12:44 +0100

    comments

--- scripts/7_deploy_staking_rewards_factory.js
+++ scripts/7_deploy_staking_rewards_factory.js
@@ -1,27 +1,26 @@
-
 async function main() {
-    const [deployer] = await ethers.getSigners();
-    const deployerAddress = await deployer.getAddress();
+  const [deployer] = await ethers.getSigners();
+  const deployerAddress = await deployer.getAddress();
 
-    console.log(
-        "Deploying staking rewards factory contract with the account:",
-        deployerAddress
-    );
+  console.log(
+    "Deploying staking rewards factory contract with the account:",
+    deployerAddress
+  );
 
-    const fuelTokenAddress = process.env.FUEL_TOKEN_ADDRESS;
+  const fuelTokenAddress = process.env.FUEL_TOKEN_ADDRESS;
 
-    const stakingRewardsFactoryFactory = await ethers.getContractFactory("StakingRewardsFactory");
-    const stakingRewardsFactory = await stakingRewardsFactoryFactory.deploy(
-      fuelTokenAddress,
-      "1601464858"
-    );
+  const stakingRewardsFactoryFactory = await ethers.getContractFactory("StakingRewardsFactory");
+  const stakingRewardsFactory = await stakingRewardsFactoryFactory.deploy(
+    fuelTokenAddress,
+    "1601464858" // THIS should be a short time in the future --- I think...
+  );
 
-    console.log('Staking rewards factory deployed at:', (await stakingRewardsFactory.deployed()).address);
+  console.log('Staking rewards factory deployed at:', (await stakingRewardsFactory.deployed()).address);
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
--- scripts/9_staking_rewards_factory_transfer.js
+++ scripts/9_staking_rewards_factory_transfer.js
@@ -3,39 +3,38 @@ const {utils} = require('ethers');
 const StakingRewardsFactory = require('../artifacts/StakingRewardsFactory.json');
 
 async function main() {
-    const [deployer] = await ethers.getSigners();
-    const deployerAddress = await deployer.getAddress();
+  const [deployer] = await ethers.getSigners();
+  const deployerAddress = await deployer.getAddress();
 
-    console.log(
-        "Staking token info with the account:",
-        deployerAddress
-    );
+  console.log(
+    "Staking token info with the account:",
+    deployerAddress
+  );
 
-    const stakingRewardsFactoryAddress = process.env.STAKING_REWARDS_FACTORY_ADDRESS;
+  const stakingRewardsFactoryAddress = process.env.STAKING_REWARDS_FACTORY_ADDRESS;
 
-    const stakingRewardsFactory = new ethers.Contract(
-      stakingRewardsFactoryAddress,
-      StakingRewardsFactory.abi,
-      deployer //provider
-    );
+  const stakingRewardsFactory = new ethers.Contract(
+    stakingRewardsFactoryAddress,
+    StakingRewardsFactory.abi,
+    deployer //provider
+  );
 
-    const stakingtokenaddress = process.env.STAKING_TOKEN_ADDRESS;
-    const res = await stakingRewardsFactory.stakingRewardsInfoByStakingToken(stakingtokenaddress);
+  const stakingtokenaddress = process.env.STAKING_TOKEN_ADDRESS;
+  const res = await stakingRewardsFactory.stakingRewardsInfoByStakingToken(stakingtokenaddress);
 
-    console.log('Staking token staking rewards contract at:', res);
+  console.log('Staking token staking rewards contract at:', res);
 
+  const tx = await stakingRewardsFactory.notifyRewardAmount(stakingtokenaddress);
 
-    const tx = await stakingRewardsFactory.notifyRewardAmount(stakingtokenaddress)
+  console.log(tx);
+  await tx.wait();
 
-    console.log(tx);
-    await tx.wait();
-
-    console.log('Staking token staking notifyRewardAmount complete');
+  console.log('Staking token staking notifyRewardAmount complete');
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
